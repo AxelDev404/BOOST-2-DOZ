e1be060b8afba1478c844fe603f4d960ed899512
Alexandru Stefan Apetroae <apetroaealexandru88@gmail.com>
2025-07-21 22:36:01 +0200

    code reorder

--- frontend/oda/src/components/TabCreate.jsx
+++ frontend/oda/src/components/TabCreate.jsx
@@ -160,14 +160,8 @@ function TabCreate(){
                             Condividi (ID utente)
                         </label>
                         <div className="relative">
-                            <input
-                                value={shared} onChange={e => setShared(Number(e.target.value))}
-                                type='number'
-                                className="block w-full px-4 py-3 bg-gray-700 border border-gray-600 rounded-lg text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all"
-                                placeholder="Id utente"
-                            />
+                            <input value={shared} onChange={e => setShared(Number(e.target.value))} type='number' className="block w-full px-4 py-3 bg-gray-700 border border-gray-600 rounded-lg text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all" placeholder="Id utente"/>
 
-                            
                             <label className="block text-sm font-medium text-gray-300 uppercase tracking-wider py-5">
                                 Lista utenti
                             </label>
@@ -176,9 +170,7 @@ function TabCreate(){
 
                                 {Array.isArray(usrlist) && usrlist.map(usrlist =>(
                              
-                                    <option key={usrlist.id} >
-                                      ID :  {usrlist.id} Username : {usrlist.username}
-                                    </option>
+                                    <option key={usrlist.id} >  ID :  {usrlist.id} Username : {usrlist.username} </option>
                                
                                 ))}
 
